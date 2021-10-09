173c0e1ed7ebb1ede21c9989d8c9ef44c085a6dd
Waddah Shamroukh <91495448+wshamroukh@users.noreply.github.com>
2021-10-09 11:42:07 +0400

    Update Create VM from VHD on Storage Account.ps1

--- Create VM from VHD on Storage Account.ps1
+++ Create VM from VHD on Storage Account.ps1
@@ -6,9 +6,7 @@ $VMSize = ''
 $Location = ''
 $StorageAccountName = ''
 $StorageAcccountSrouceContainer = ''
-$StorageAccountDestContainer = ''
 $OSVHDName = ''
-$DataVHDName = ''
 
 # Login to the destination subscription where you have copied the vhd to a storage account
 $sub = Connect-AzAccount -ErrorAction Stop
@@ -20,17 +18,15 @@ $sub = Connect-AzAccount -ErrorAction Stop
 $StorageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
 $VNet = Get-AzVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroupName
 $Subnet = $VNet.Subnets |Where-Object {$_.Name -eq $SubnetName}
-$NIC = New-AzNetworkInterface -Name "$VMName-NIC" -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $Subnet.Id
-$credentials = Get-Credential
-$VM = New-AzVMConfig -VMName $VMName -VMSize $VMSize
-$VM = Set-AzVMOperatingSystem -VM $VM -Linux -ComputerName $VMName -Credential $credentials
-$VM = Add-AzVMNetworkInterface -VM $VM -Id $NIC.Id
+$Subnet2 = $VNet.Subnets |Where-Object {$_.Name -eq 'vms'}
+$pip = New-AzPublicIpAddress -Name "$VMName-pub-ip" -ResourceGroupName $ResourceGroupName -Location $Location -Sku Basic -AllocationMethod Static
+$NIC = New-AzNetworkInterface -Name "$VMName-ext-nic" -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $Subnet.Id -EnableIPForwarding -PublicIpAddressId $pip.Id
+$NIC2 = New-AzNetworkInterface -Name "$VMName-int-nic" -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $Subnet2.Id -EnableIPForwarding
 $SourceOSDiskURI = $StorageAccount.PrimaryEndpoints.Blob + "$StorageAcccountSrouceContainer/" + $OSVHDName
-$SourceDataDiskURI = $StorageAccount.PrimaryEndpoints.Blob + "$StorageAcccountSrouceContainer/" + $DataVHDName
-$DestOSDiskURI = $StorageAccount.PrimaryEndpoints.Blob + "$StorageAccountDestContainer/$OSVHDName"
-$DestDataDiskURI = $StorageAccount.PrimaryEndpoints.Blob + "$StorageAccountDestContainer/$DataVHDName"
-$VM = Set-AzVMOSDisk -VM $VM -SourceImageUri $SourceOSDiskURI -VhdUri $DestOSDiskURI -Name $OSVHDName -CreateOption FromImage -Linux
-$VM = Add-AzVMDataDisk -VM $VM -Name $DataVHDName -VhdUri $DestDataDiskURI -Lun 0 -SourceImageUri $SourceDataDiskURI -CreateOption FromImage
-
+$diskConfig = New-AzDiskConfig -SkuName Standard_LRS -Location $Location -CreateOption Import -StorageAccountId $StorageAccount.Id -SourceUri $SourceOSDiskURI -OsType Linux
+$osdisk = New-AzDisk -Disk $diskConfig -ResourceGroupName $ResourceGroupName -DiskName "$VMName-osdisk"
+$VM = New-AzVMConfig -VMName $VMName -VMSize $VMSize
+$VM = Add-AzVMNetworkInterface -VM $VM -Id $NIC.Id -Primary
+$VM = Add-AzVMNetworkInterface -VM $VM -Id $NIC2.Id
+$VM = Set-AzVMOSDisk -ManagedDiskId $osdisk.Id -VM $VM -CreateOption Attach -Linux
 New-AzVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VM -Verbose
-
